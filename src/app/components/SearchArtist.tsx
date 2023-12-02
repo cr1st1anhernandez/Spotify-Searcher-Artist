99c6c1b8a15d73f8fc5664ede88afd798d04b5f8
cristian <cristian0402218@gmail.com>
2023-12-02 08:34:01 -0600

    :sparkles: Add artist search component

---
+++ src/app/components/SearchArtist.tsx
@@ -0,0 +1,8 @@
+export default function SearchArtist(): JSX.Element {
+  return (
+    <input
+      type="text"
+      className="h-10 w-full rounded-md border-2 border-zinc-400 bg-zinc-200 px-4 outline-none focus:border-zinc-700 dark:border-zinc-500 dark:bg-zinc-700 dark:focus:border-zinc-100"
+    />
+  )
+}
